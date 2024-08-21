051be554bff25dfecf93a5a90b810f732cea8b00
alemoles <alemolerio@gmail.com>
2024-08-20 21:46:40 -0300

    set up Phaser config

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO, // webgl, canvas
+    width: 256,
+    height: 244,
+    backgroundColor: '#fff',
+    parent: 'game',
+    scene: {
+        preload, // precargar recursos del juego
+        create, // se ejecuta cuando el juego comienza
+        update // se ejecuta en cada frame
+    }
+}
+
+new Phaser.Game(config)
+
+function preload() {
+    console.log('preload');
+}
+
+function create() {
+    console.log('create');
+}
+
+function update() {
+    console.log('update');
+}
